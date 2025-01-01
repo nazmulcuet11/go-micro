13be74dd04c8da55f1a561779c09ec3838c8bee1
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-01-01 19:43:31 +0700

    connect to rabbitmq

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"time"
 
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	rabbitconn, err := connect()
+	if err != nil {
+		log.Panic(err)
+	}
+	defer rabbitconn.Close()
+
+	log.Println("connected to rabbitmq")
+}
+
+func connect() (*amqp.Connection, error) {
+	count := 0
+	var connection *amqp.Connection
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			count++
+			if count > 10 {
+				fmt.Println("Could not connect to rabbitmq")
+				return nil, err
+			} else {
+				fmt.Println("RabbitMQ not yet ready, backingOff for 1 sec")
+				time.Sleep(1 * time.Second)
+			}
+		} else {
+			connection = c
+			break
+		}
+	}
+	return connection, nil
 }
